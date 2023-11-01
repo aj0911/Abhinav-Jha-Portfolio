266ad3d6c2ea5b9e421023e0bd3abbb5634a8c3b
abhinav jha <jhaabhinav16@gmail.com>
2023-11-01 20:21:53 +0530

    project started

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useRef } from 'react'
+import './Header.css'
+
+const Header = () => {
+    const menus = [
+        'About',
+        'Education',
+        'Experience',
+        'Skills',
+        'Projects',
+        'Testimonials',
+        'Contact'
+    ]
+    const header = useRef()
+    document.addEventListener('scroll',()=>{
+        if(window.scrollY>20){
+            header.current.classList.add('sticky')
+        }
+        if(window.scrollY<20){
+            header.current.classList.remove('sticky')
+        }
+    })
+  return (
+    <header ref={header}>
+        <a href='#About' className="logo">Aj.</a>
+        <nav>
+            {
+                menus.map((menu,index)=>(
+                    <a key={index} href={`#${menu}`} className='menu'><span>{index+1}.</span>{menu}</a>
+                ))
+            }
+            <button>Resume</button>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
